398f2fe092e232ad25af21a9118e294db2c30c1f
Chris Siena <zenatron@gmail.com>
2018-03-26 19:18:34 -0400

    debugging

--- src/greph.java
+++ src/greph.java
@@ -12,15 +12,20 @@ public class greph {
 	}
 	
 	public greph(String[] args) {
+		int error = 0;
+		if (Arrays.asList(args).contains("-debug")) {
+			debug = true;
+			debug("Enabling Debug mode...");
+		}
 		for (int i = 0; i < args.length; i++) {
-			output(args[i]);
+			debug("Current parameter: " + args[i]);
 			try {
 				if (args[i].equals("-r")) {
-					output(args[i+1]);
 					if ((args[i+1].equals("-d")) || (args[i+1].equals("-n")) || (args[i+1].equals("-f")) || (args[i+1].equals("-h")) || (args[i+1].equals("-help"))) {
 						output("ERROR: The RegEx argument is missing from the function call.");
 						return;
 					} else {
+						debug("Setting RegEx to " + args[i+1]);
 						regEx = args[i+1];
 						i++;
 					}
@@ -29,6 +34,7 @@ public class greph {
 						output("ERROR: The input file argument is missing from the function call.");
 						return;
 					} else {
+						debug("Setting the input file to " + args[i+1]);
 						inputFile = args[i+1];
 						i++;
 					}
@@ -37,6 +43,7 @@ public class greph {
 						output("ERROR: The NFA file name argument is missing from the function call.");
 						return;
 					} else {
+						debug("Setting NFA output file name to " + args[i+1]);
 						nfaName = args[i+1];
 						i++;
 					}
@@ -45,16 +52,20 @@ public class greph {
 						output("ERROR: The DFA file name argument is missing from the function call.");
 						return;
 					} else {
+						debug("Setting DFA output file name to " + args[i+1]);
 						dfaName = args[i+1];
 						i++;
 					}
+				} else if (args[i].equals("-debug")) {
+					//holds the debug parameter slot
 				} else if ((args[i].equals("-h")) || args[i].equals("-help")) {
 					output("Grephy\n"
 						+  "-r 'RegEx'         -- The input RegEx (required)\n"
 						+  "-f 'input file'    -- The input file the RegEx will check (required)\n"
 						+  "-n 'nfa file name' -- The name of the nfa output file (optinal)\n"
 						+  "-d 'dfa file name' -- The name of the dfa output file (optinal)\n"
-						+  "-h -help           -- Brings up this help menu\n");
+						+  "-h -help           -- Brings up this help menu\n"
+						+  "-debug             -- Enables debug mode\n");
 					return;
 				}
 			} catch (Exception e) {
