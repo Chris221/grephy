4e7b1cbd5bc9c1bebbbc845c694ea16b5e7eb026
Chris Siena <zenatron@gmail.com>
2018-03-27 08:57:35 -0400

    errors handled by the output class

--- src/output.java
+++ src/output.java
@@ -8,6 +8,8 @@ public class output {
 	public Boolean debug = false; 
 	public DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");  
 	public LocalDateTime now = LocalDateTime.now(); 
+	public int error = 0;
+	
 	/*
 	 * For outputting debug information to the console
 	 */
@@ -16,12 +18,23 @@ public class output {
 			output("Debugger: " + text);
 		}
 	}
+	
+	/*
+	 * For outputting errors to the console
+	 */
+	public void error(String text) {
+		output("ERROR: " + text);
+		error++;
+	}
+	
 	/*
 	 * For outputting to the console
 	 */
+	@SuppressWarnings("all")
 	public void output(String text) {
 		System.out.println(dtf.format(now) + ": " + text);
 	}
+	@SuppressWarnings("all")
 	public void output(int text) {
 		System.out.println(dtf.format(now) + ": " + text);
 	}
