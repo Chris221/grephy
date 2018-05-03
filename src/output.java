536ea120d90459488e4cf1e4391e51f4c498cc90
Chris Siena <zenatron@gmail.com>
2018-05-02 20:26:06 -0400

    Comments for output

--- src/output.java
+++ src/output.java
@@ -4,7 +4,7 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class output {
-
+	//Defines the required variables 
 	public Boolean debug = false; 
 	public DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");  
 	public LocalDateTime now = LocalDateTime.now(); 
